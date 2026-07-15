4a548870a031e44f58cf71649faf04598bcc72a6
saddab <saddabakhtar17@gmail.com>
2026-07-15 19:02:26 +0530

    fix: separate load-balanced rest client for product service client

--- Order/src/main/java/com/sadcodes/ecommerce/order/client/ProductServiceClientConfig.java
+++ Order/src/main/java/com/sadcodes/ecommerce/order/client/ProductServiceClientConfig.java
@@ -1,25 +1,35 @@
 package com.sadcodes.ecommerce.order.client;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.Optional;
 
 @Configuration
 class ProductServiceClientConfig {
 
     @Bean
-    @LoadBalanced
+    @Primary
     public RestClient.Builder restClientBuilder() {
         return RestClient.builder();
     }
 
     @Bean
-    public ProductServiceClient restClientInterface(RestClient.Builder restClientBuilder) {
-        RestClient restClient = restClientBuilder
-                .baseUrl("http://product").build();
+    @LoadBalanced
+    public RestClient.Builder productServiceRestClientBuilder() {
+        return RestClient.builder();
+    }
+
+    @Bean
+    public ProductServiceClient restClientInterface(@Qualifier("productServiceRestClientBuilder") RestClient.Builder restClientBuilder) {
+        RestClient restClient = restClientBuilder.baseUrl("http://product").defaultStatusHandler(HttpStatusCode::is4xxClientError, ((request, response) -> Optional.empty())).build();
 
         RestClientAdapter adapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
